fba01af68e38ef1548650d7adde3c33be8197b7a
shlyapo <shlyapoelizzz@gmail.com>
2022-06-09 15:01:50 +0300

    lab4

--- NowSalary/settings.py
+++ NowSalary/settings.py
@@ -135,6 +135,8 @@ LOGOUT_URL = reverse_lazy('logout')
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
